cc5c3db84a06fdd8ff10f3ce2a1cade7fab87e05
massebateke <massebateke4@gmail.com>
2024-01-27 00:24:48 +0100

    V4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const musicContainer = document.querySelector('.music-container');//en gros ca affiche le txt de la classe donné ou le contenenu 
+const musicContainer = document.querySelector('.music-container');
 const playBtn = document.querySelector('#play');
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
@@ -9,34 +9,80 @@ const title = document.querySelector('#title');
 const cover = document.querySelector('#cover');
 
 //Song title
-const songs = ['hey','summer','ukulele'];
+const songs = ['CYANIDE REMIX (feat. Koffee)','Don Toliver - No Idea','SZA - Used ft. Don Toliver','Flo Milli - Never Lose Me','Jungle - Back On 74','SAULT - Masterpiece'];
 
 //Keep tracks of song
-let songIndex = 2; //means thet ukulele is the default song
+let songIndex = 0; 
 
 //Initially load song into the document object model
 loadSong(songs[songIndex]);
 
 //Update song details
 function loadSong(song){
-    title.innerText = song;//changement du titre du song en fonction de l'array
-    audio.src = 'music/${song}.mp3';//chemin vers le song
-    cover.src = 'images/${cover}.jpg';//ou song
+    title.innerText = song
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`                             
 }
 
 function playSong(){
     musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
 }
 
 function pauseSong(){
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+function prevSong(){
+    songIndex--
+
+    if(songIndex < 0){
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function nextSong(){
+    songIndex++
+
+    if(songIndex > songs.length + 1){
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function updateProgress(e){  
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
 
 }
 
 
-
-
 //Event listeners
-playBtn.addEventListener('click', () => { //en gros si tu fais une action il ya une action
+playBtn.addEventListener('click', () => { 
     const isPlaying = musicContainer.classList.contains('play');
 
     if(isPlaying){
@@ -46,3 +92,12 @@ playBtn.addEventListener('click', () => { //en gros si tu fais une action il ya 
     }
     
 })
+
+//Change song event
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
